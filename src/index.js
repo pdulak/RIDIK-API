6ddff4c5da7dbbb8b9f3f2828a3552e3115fc3da
Pawel Dulak <pawel.dulak@gmail.com>
2023-05-04 22:42:32 +0200

    chore: initial commit

---
+++ src/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const morgan = require('morgan');
+const dotenv = require('dotenv');const app = express();
+app.use(express.json());
+
+// Load environment variables from .env file
+dotenv.config();
+
+app.post('/upload', (req, res) => {
+    const fileUrl = req.body.fileUrl;
+
+    // Do something with the fileUrl here...
+
+    res.json({ success: true });
+});
+
+app.use(morgan('dev'));
+
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
